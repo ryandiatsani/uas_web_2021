73b9a417fe5a7505bf6e5995449a3f448696da4b
Ryandi Atsani <31480387+ryandiatsani@users.noreply.github.com>
2021-01-25 18:01:07 +0800

    Update about.php

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
     <div class="col-8">
 
         <p>Dibuat oleh 1810520035-LALU AHMAD GEDE PARIANDI ATSANi - Menggunakan boostrap 4</p>
-        <p>website ini diperuntukan untuk UAS pemograman web</p>
+        <p>website ini diperuntukan untuk UAS pemograman web(Tema : promosi barang)</p>
 
         <p>website ini bertipe kontribusi, dimana setiap pengguna yang menggunakan website ini bisa merubah segala data yang ada, pengguna dapat menambah dan mempromosikan suatu barang, pengguna yang lain juga bisa merubah isi dari promosi untuk menambahkan penjelasan yang mungkin belum sempat di jelaskan pembuat, lalu pengguna juga dapat menghapus barang promosi yang dinilai tidak pantas untuk dilihat karena menggandung unsur tertentu yang tidak di perbolehkan</p>
         <p> pengguna juga bisa menambakan barang ke halaman favorite untuk akses yang lebih cepat, dan menghapusnya jika sudah tidak diperlukan</p>
